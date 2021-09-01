e177f75dd33ca2fcd730069c60d50ee1d6df5e1e
Benoit Delemps <benoitdelemps@gmail.com>
2021-09-01 10:40:11 +0200

    fallback to default_output_config when OutputBuilder::build

--- src/context.rs
+++ src/context.rs
@@ -292,7 +292,34 @@ impl AudioContext {
         let render = RenderThread::new(sample_rate, channels as usize, receiver);
         let stream = io_builder.build(render);
 
-        Self { base, stream }
+        match stream {
+            Err(_) => {
+                let io_builder = io::OutputBuilder::default();
+                let config = io_builder.config();
+                log::debug!("Output {:?}", config);
+
+                let sample_rate = SampleRate(config.sample_rate.0);
+                let channels = config.channels as u32;
+
+                // communication channel to the render thread
+                let (sender, receiver) = crossbeam_channel::unbounded();
+
+                // first, setup the base audio context
+                let base = BaseAudioContext::new(sample_rate, channels, sender);
+
+                // spawn the render thread
+                let render = RenderThread::new(sample_rate, channels as usize, receiver);
+                let stream = io_builder
+                    .build(render)
+                    .expect("Stream default build failed");
+
+                Self { base, stream }
+            }
+            Ok(stream) => {
+                stream.play().expect("Stream play failed");
+                Self { base, stream }
+            }
+        }
     }
 
     #[cfg(test)] // in tests, do not set up a cpal Stream
