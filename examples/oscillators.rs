f24d1969aeca71c18f2951e7933c84f5dc5d79bd
Benoit Delemps <benoitdelemps@gmail.com>
2021-09-28 09:47:59 +0200

    better oscillators example

--- examples/oscillators.rs
+++ examples/oscillators.rs
@@ -1,28 +1,83 @@
 //! This example plays each oscillator type sequentially
 use web_audio_api::context::{AsBaseAudioContext, AudioContext};
-use web_audio_api::node::{AudioNode, AudioScheduledSourceNode, OscillatorType};
+use web_audio_api::node::{
+    AudioNode, AudioScheduledSourceNode, OscillatorType, PeriodicWaveOptions,
+};
 
 fn main() {
     // Create an audio context where all audio nodes lives
     let context = AudioContext::new();
 
     // Create an oscillator node with sine (default) type
-    let osc = context.create_oscillator();
+    let mut osc = context.create_oscillator();
 
     // Connect osc to the destination node which is the default output device
     osc.connect(&context.destination());
 
+    // defined periodic wave characteristics
+    let options = Some(PeriodicWaveOptions {
+        real: vec![0., 0.5, 0.5],
+        imag: vec![0., 0., 0.],
+        disable_normalization: false,
+    });
+
+    // Create a custom periodic wave
+    let periodic_wave = context.create_periodic_wave(options);
+
     // Oscillator needs to be started explicitily
     osc.start();
 
-    // enjoy listening
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    println!("Sine tone sweep playing...");
+
+    // Sine tone sweep
+    for i in 0..1000 {
+        osc.frequency().set_value(440. + i as f32);
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
+
+    println!("Square tone sweep playing...");
+
+    // Select Square as the oscillator type
     osc.set_type(OscillatorType::Square);
-    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // Square tone sweep
+    for i in 0..1000 {
+        osc.frequency().set_value(440. + i as f32);
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
+
+    println!("Triangle tone sweep playing...");
+
+    // Select Triangle as the oscillator type
     osc.set_type(OscillatorType::Triangle);
-    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // Triangle tone sweep
+    for i in 0..1000 {
+        osc.frequency().set_value(440. + i as f32);
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
+
+    println!("Sawtooth tone sweep playing...");
+
+    // Select Sawtooth as the oscillator type
     osc.set_type(OscillatorType::Sawtooth);
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    osc.set_type(OscillatorType::Sine);
-    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // Sawtooth tone sweep
+    for i in 0..1000 {
+        osc.frequency().set_value(440. + i as f32);
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
+
+    println!("Periodic wave tone sweep playing...");
+
+    // Select Sawtooth as the PeriodicWave type
+    osc.set_periodic_wave(periodic_wave);
+
+    // Custom periodic wave tone sweep
+    for i in 0..1000 {
+        osc.frequency().set_value(440. + i as f32);
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
+
+    // enjoy listening
 }
