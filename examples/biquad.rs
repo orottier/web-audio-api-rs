db129f3a20870481996497c4173b34e38d53bca6
Benoit Delemps <benoitdelemps@gmail.com>
2021-10-04 17:31:17 +0200

    add biquad example

---
+++ examples/biquad.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use web_audio_api::context::{AsBaseAudioContext, AudioContext};
+use web_audio_api::media::{MediaElement, OggVorbisDecoder};
+use web_audio_api::node::{AudioControllableSourceNode, AudioNode, AudioScheduledSourceNode};
+
+fn main() {
+    let context = AudioContext::new();
+
+    // setup background music:
+    // read from local file
+    let file = File::open("sample.ogg").unwrap();
+    // decode file to media stream
+    let stream = OggVorbisDecoder::try_new(file).unwrap();
+    // wrap stream in MediaElement, so we can control it (loop, play/pause)
+    let media = MediaElement::new(stream);
+    // register as media element in the audio context
+    let background = context.create_media_element_source(media);
+
+    // use a gain node to control volume
+    let gain = context.create_gain();
+    // play at low volume
+    gain.gain().set_value(0.5);
+
+    // create a biquad filter
+    let biquad = context.create_biquad_filter();
+
+    // connect the media node to the gain node
+    background.connect(&gain);
+    // connect the gain node to the biquad node
+    gain.connect(&biquad);
+    // connect the biquad node to the destination node (speakers)
+    biquad.connect(&context.destination());
+
+    // start playback
+    background.set_loop(true);
+    background.start();
+
+    // enjoy listening
+    std::thread::sleep(std::time::Duration::from_secs(4));
+}
