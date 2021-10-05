967de63330e610e67913c01cb3462e9a8b9cfa57
Benoit Delemps <benoitdelemps@gmail.com>
2021-10-05 10:26:57 +0200

    better example

--- examples/biquad.rs
+++ examples/biquad.rs
@@ -15,29 +15,34 @@ fn main() {
     let media = MediaElement::new(stream);
     // register as media element in the audio context
     let background = context.create_media_element_source(media);
-
-    // use a gain node to control volume
-    let gain = context.create_gain();
-    // play at low volume
-    gain.gain().set_value(0.5);
-
     // create a biquad filter
     let biquad = context.create_biquad_filter();
-
     // connect the media node to the gain node
-    background.connect(&gain);
-    // connect the gain node to the biquad node
-    gain.connect(&biquad);
+    background.connect(&biquad);
     // connect the biquad node to the destination node (speakers)
     biquad.connect(&context.destination());
 
-    let frequency_hz = [500.0, 1000.];
-    let mut mag_response = [0.; 2];
-    let mut phase_response = [0.; 2];
+    let frequency_hz = [250., 500.0, 750.0, 1000., 1500.0, 2000.0, 4000.0];
+    let mut mag_response = [0.; 7];
+    let mut phase_response = [0.; 7];
 
     biquad.get_frequency_response(&frequency_hz, &mut mag_response, &mut phase_response);
 
-    println!("{:?}", mag_response);
+    println!("=================================");
+    println!("Biquad filter frequency response:");
+    println!("=================================");
+    println!("Cutoff freq -- {} Hz", biquad.frequency().value());
+    println!("Gain -- {}", biquad.gain().value());
+    println!("Q factor -- {}", biquad.q().value());
+    println!("---------------------------------");
+    for i in 0..frequency_hz.len() {
+        println!(
+            "{} Hz --> {} dB",
+            frequency_hz[i],
+            20.0 * mag_response[i].log10()
+        );
+    }
+    println!("---------------------------------");
 
     // start playback
     background.set_loop(true);
