a9b7b530786498608c82b77f1fa1fb167be7db82
Otto <ottorottier@gmail.com>
2021-12-08 19:39:57 +0100

    Setting up the control side of DelayNode - split in reader and writer

--- src/node/delay.rs
+++ src/node/delay.rs
@@ -7,6 +7,9 @@ use crate::{SampleRate, BUFFER_SIZE};
 
 use super::AudioNode;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 /// Options for constructing a DelayNode
 pub struct DelayOptions {
     pub max_delay_time: f32,
@@ -25,15 +28,26 @@ impl Default for DelayOptions {
 }
 
 /// Node that delays the incoming audio signal by a certain amount
+/*
+ * For simplicity in the audio graph rendering, we have made the conscious decision to deviate from
+ * the spec and split the delay node up front in a reader and writer node (instead of during the
+ * render loop - see https://webaudio.github.io/web-audio-api/#rendering-loop )
+ */
 pub struct DelayNode {
-    registration: AudioContextRegistration,
+    reader_registration: AudioContextRegistration,
+    writer_registration: AudioContextRegistration,
     delay_time: AudioParam,
     channel_config: ChannelConfig,
 }
 
 impl AudioNode for DelayNode {
+    /*
+     * We set the writer node as 'main' registration.  This means other nodes can say
+     * `node.connect(delaynode)` and they will connect to the writer.
+     * Below, we override the (dis)connect methods as they should operate on the reader node.
+     */
     fn registration(&self) -> &AudioContextRegistration {
-        &self.registration
+        &self.writer_registration
     }
 
     fn channel_config_raw(&self) -> &ChannelConfig {
@@ -46,41 +60,92 @@ impl AudioNode for DelayNode {
     fn number_of_outputs(&self) -> u32 {
         1
     }
+
+    /// Connect a specific output of this AudioNode to a specific input of another node.
+    fn connect_at<'a>(
+        &self,
+        dest: &'a dyn AudioNode,
+        output: u32,
+        input: u32,
+    ) -> Result<&'a dyn AudioNode, crate::IndexSizeError> {
+        if self.context() != dest.context() {
+            panic!("attempting to connect nodes from different contexts");
+        }
+
+        if self.number_of_outputs() <= output || dest.number_of_inputs() <= input {
+            return Err(crate::IndexSizeError {});
+        }
+
+        self.context()
+            .connect(self.reader_registration.id(), dest.id(), output, input);
+
+        Ok(dest)
+    }
+
+    /// Disconnects all outputs of the AudioNode that go to a specific destination AudioNode.
+    fn disconnect<'a>(&self, dest: &'a dyn AudioNode) -> &'a dyn AudioNode {
+        if self.context() != dest.context() {
+            panic!("attempting to disconnect nodes from different contexts");
+        }
+
+        self.context()
+            .disconnect(self.reader_registration.id(), dest.id());
+
+        dest
+    }
+
+    /// Disconnects all outgoing connections from the AudioNode.
+    fn disconnect_all(&self) {
+        self.context().disconnect_all(self.reader_registration.id());
+    }
 }
 
 impl DelayNode {
     pub fn new<C: AsBaseAudioContext>(context: &C, options: DelayOptions) -> Self {
-        context.base().register(move |registration| {
-            let param_opts = AudioParamOptions {
-                min_value: 0.,
-                max_value: options.max_delay_time,
-                default_value: 0.,
-                automation_rate: crate::param::AutomationRate::A,
-            };
-            let (param, proc) = context
-                .base()
-                .create_audio_param(param_opts, registration.id());
+        // allocate large enough buffer to store all delayed samples
+        let max_samples = options.max_delay_time * context.base().sample_rate().0 as f32;
+        let max_quanta = (max_samples.ceil() as usize + BUFFER_SIZE - 1) / BUFFER_SIZE;
+        let delay_buffer = Vec::with_capacity(max_quanta);
 
-            param.set_value_at_time(options.delay_time, 0.);
+        let shared_buffer = Rc::new(RefCell::new(delay_buffer));
+        let shared_buffer_clone = shared_buffer.clone();
 
-            // allocate large enough buffer to store all delayed samples
-            let max_samples = options.max_delay_time * context.base().sample_rate().0 as f32;
-            let max_quanta = (max_samples.ceil() as usize + BUFFER_SIZE - 1) / BUFFER_SIZE;
-            let delay_buffer = Vec::with_capacity(max_quanta);
+        context.base().register(move |writer_registration| {
+            let node = context.base().register(move |reader_registration| {
+                let param_opts = AudioParamOptions {
+                    min_value: 0.,
+                    max_value: options.max_delay_time,
+                    default_value: 0.,
+                    automation_rate: crate::param::AutomationRate::A,
+                };
+                let (param, proc) = context
+                    .base()
+                    .create_audio_param(param_opts, reader_registration.id());
 
-            let render = DelayRenderer {
-                delay_time: proc,
-                delay_buffer,
+                param.set_value_at_time(options.delay_time, 0.);
+
+                let reader_render = DelayReader {
+                    delay_time: proc,
+                    delay_buffer: shared_buffer_clone,
+                    index: 0,
+                };
+
+                let node = DelayNode {
+                    reader_registration,
+                    writer_registration,
+                    channel_config: options.channel_config.into(),
+                    delay_time: param,
+                };
+
+                (node, Box::new(reader_render))
+            });
+
+            let writer_render = DelayWriter {
+                delay_buffer: shared_buffer,
                 index: 0,
             };
 
-            let node = DelayNode {
-                registration,
-                channel_config: options.channel_config.into(),
-                delay_time: param,
-            };
-
-            (node, Box::new(render))
+            (node, Box::new(writer_render))
         })
     }
 
@@ -89,18 +154,24 @@ impl DelayNode {
     }
 }
 
-struct DelayRenderer {
+struct DelayReader {
     delay_time: AudioParamId,
-    delay_buffer: Vec<AudioBuffer>,
+    delay_buffer: Rc<RefCell<Vec<AudioBuffer>>>,
+    index: usize,
+}
+
+struct DelayWriter {
+    delay_buffer: Rc<RefCell<Vec<AudioBuffer>>>,
     index: usize,
 }
 
 // SAFETY:
 // AudioBuffers are not Send but we promise the `delay_buffer` Vec is empty before we ship it to
 // the render thread.
-unsafe impl Send for DelayRenderer {}
+unsafe impl Send for DelayReader {}
+unsafe impl Send for DelayWriter {}
 
-impl AudioProcessor for DelayRenderer {
+impl AudioProcessor for DelayWriter {
     fn process(
         &mut self,
         inputs: &[AudioBuffer],
@@ -109,6 +180,24 @@ impl AudioProcessor for DelayRenderer {
         _timestamp: f64,
         sample_rate: SampleRate,
     ) -> bool {
+        todo!()
+    }
+}
+
+impl AudioProcessor for DelayReader {
+    fn process(
+        &mut self,
+        inputs: &[AudioBuffer],
+        outputs: &mut [AudioBuffer],
+        params: AudioParamValues,
+        _timestamp: f64,
+        sample_rate: SampleRate,
+    ) -> bool {
+        todo!()
+    }
+}
+
+/*
         // single input/output node
         let input = &inputs[0];
         let output = &mut outputs[0];
@@ -135,5 +224,4 @@ impl AudioProcessor for DelayRenderer {
 
         // todo: return false when all inputs disconnected and buffer exhausted
         true
-    }
-}
+*/
