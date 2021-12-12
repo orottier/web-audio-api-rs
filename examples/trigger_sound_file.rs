45d74217d15a724df176fe1e8032cd1a2c83d52f
b-ma <benjamin.matuszewski@gmail.com>
2021-12-12 23:17:01 +0100

    pipe in gain

--- examples/trigger_sound_file.rs
+++ examples/trigger_sound_file.rs
@@ -9,16 +9,26 @@ use web_audio_api::audio_buffer::{AudioBuffer, decode_audio_data};
 
 fn main() {
   let context = AudioContext::new(None);
+  // @note - `resume` does not seem to be needed
 
   // load and decode buffer
   let file = std::fs::File::open("sample.wav").unwrap();
   let audio_buffer = decode_audio_data(file);
 
-  let mut src = context.create_buffer_source();
-  src.set_buffer(&audio_buffer);
-  src.connect(&context.destination());
-  src.start();
+  for i in 0..9 {
+    // let offset = i as f64 / 1000.; // 10ms - look ok in --release
+    let offset = i as f64 / 2.;
 
-  // context.start_rendering();
-  std::thread::sleep(std::time::Duration::from_secs(5));
+    let gain = if i % 4 == 0 { 1. } else { 0.3 };
+    let env = context.create_gain();
+    env.gain().set_value(gain);
+    env.connect(&context.destination());
+
+    let mut src = context.create_buffer_source();
+    src.set_buffer(&audio_buffer);
+    src.connect(&env);
+    src.start_at(context.current_time() + offset);
+  }
+
+  std::thread::sleep(std::time::Duration::from_secs(8));
 }
