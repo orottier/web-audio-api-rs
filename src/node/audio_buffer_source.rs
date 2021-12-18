fffa831e047804bc978ce73923c3ef82f6a39c01
b-ma <benjamin.matuszewski@gmail.com>
2021-12-18 07:49:12 +0100

    `no_run` tag instead of `rust`

--- src/node/audio_buffer_source.rs
+++ src/node/audio_buffer_source.rs
@@ -45,7 +45,7 @@ struct AudioBufferMessage(AudioBuffer);
 ///
 /// # Usage
 ///
-/// ```rust
+/// ```no_run
 /// use std::fs::File;
 /// use web_audio_api::context::{AsBaseAudioContext, AudioContext};
 /// use web_audio_api::node::AudioNode;
