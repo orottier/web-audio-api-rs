42fe0c53c06a4abeabf03093df7d0f722a1e5a66
b-ma <benjamin.matuszewski@gmail.com>
2021-12-28 08:52:37 +0100

    uncommented 1 quantum test

--- src/node/delay.rs
+++ src/node/delay.rs
@@ -700,30 +700,30 @@ mod tests {
         // to test multiple times since (currently) the topological sort of the
         // graph depends on randomized hash values. This bug only occurs when the
         // Writer is called earlier than the Reader. 10 times should do:
-        // for _ in 0..10 { // set delay and max delay time exactly 1 render quantum
-        //     let sample_rate = SampleRate(128);
-        //     let mut context = OfflineAudioContext::new(1, 256, sample_rate);
+        for _ in 0..10 { // set delay and max delay time exactly 1 render quantum
+            let sample_rate = SampleRate(128);
+            let mut context = OfflineAudioContext::new(1, 256, sample_rate);
 
-        //     let delay = context.create_delay(1.);
-        //     delay.delay_time.set_value(1.);
-        //     delay.connect(&context.destination());
+            let delay = context.create_delay(1.);
+            delay.delay_time.set_value(1.);
+            delay.connect(&context.destination());
 
-        //     let mut dirac = context.create_buffer(1, 1, sample_rate);
-        //     dirac.copy_to_channel(&[1.], 0);
+            let mut dirac = context.create_buffer(1, 1, sample_rate);
+            dirac.copy_to_channel(&[1.], 0);
 
-        //     let mut src = context.create_buffer_source();
-        //     src.connect(&delay);
-        //     src.set_buffer(&dirac);
-        //     src.start_at(0.);
+            let mut src = context.create_buffer_source();
+            src.connect(&delay);
+            src.set_buffer(&dirac);
+            src.start_at(0.);
 
-        //     let result = context.start_rendering();
-        //     let channel = result.get_channel_data(0);
+            let result = context.start_rendering();
+            let channel = result.get_channel_data(0);
 
-        //     let mut expected = vec![0.; 256];
-        //     expected[128] = 1.;
+            let mut expected = vec![0.; 256];
+            expected[128] = 1.;
 
-        //     assert_float_eq!(channel[..], expected[..], abs_all <= 0.);
-        // }
+            assert_float_eq!(channel[..], expected[..], abs_all <= 0.);
+        }
 
         for _ in 0..10 {
             // set delay and max delay time exactly 2 render quantum
