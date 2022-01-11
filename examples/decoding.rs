62a2380eae75a7ffc4f9adc8d8777098f64dc986
b-ma <benjamin.matuszewski@gmail.com>
2022-01-11 09:40:42 +0100

    improved decoding example

--- examples/decoding.rs
+++ examples/decoding.rs
@@ -4,33 +4,48 @@ use web_audio_api::node::AudioNode;
 
 fn main() {
     let files = [
+        "samples/sample-faulty.wav",
         "samples/sample.wav",
         "samples/sample.flac",
         "samples/sample.ogg",
         "samples/sample.mp3",
-        // not supported by decoder:
-        // "samples/sample.aiff",
-        // "samples/sample.webm", // 48kHz,
+        // cannot decode, format not supported or file corrupted
+        "samples/empty_2c.wav",
+        "samples/corrupt.wav",
+        "samples/sample.aiff",
+        "samples/sample.webm", // 48kHz,
     ];
 
     let audio_context = AudioContext::new(None);
 
     for filepath in files.iter() {
         println!("> --------------------------------");
-        println!("> reading file: {:?}", filepath);
 
         let file = File::open(filepath).unwrap();
-        let buffer = audio_context.decode_audio_data(file).unwrap();
+        let res = audio_context.decode_audio_data(file);
 
-        println!("> duration: {:?}", buffer.duration());
-        println!("> num samples: {:?}", buffer.get_channel_data(0).len());
-        println!("> --------------------------------");
+        match res {
+            Ok(buffer) => {
+                println!("> playing file: {:?}", filepath);
+                println!("> duration: {:?}", buffer.duration());
+                println!("> length: {:?}", buffer.length());
+                println!("> channels: {:?}", buffer.number_of_channels());
+                println!("> sample rate: {:?}", buffer.sample_rate());
+                println!("> --------------------------------");
 
-        let src = audio_context.create_buffer_source();
-        src.connect(&audio_context.destination());
-        src.set_buffer(buffer);
-        src.start();
+                let src = audio_context.create_buffer_source();
+                src.connect(&audio_context.destination());
+                src.set_buffer(buffer);
+                src.start();
 
-        std::thread::sleep(std::time::Duration::from_secs(4));
+                std::thread::sleep(std::time::Duration::from_secs(4));
+            },
+            Err(e) => {
+                println!("> Error decoding audio file: {:?}", filepath);
+                eprintln!("> {:?}", e);
+                println!("> --------------------------------");
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+        }
     }
 }
