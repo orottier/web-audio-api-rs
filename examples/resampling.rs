c6516d15e3fb0cd352b343ab9f1aed6cff13dcd7
b-ma <benjamin.matuszewski@gmail.com>
2022-02-14 17:15:13 +0100

    clippy

--- examples/resampling.rs
+++ examples/resampling.rs
@@ -4,12 +4,14 @@ use web_audio_api::node::AudioNode;
 
 fn main() {
     let audio_context = AudioContext::new(None);
-    println!("> AudioContext sample_rate: {:?}", audio_context.sample_rate());
+    println!(
+        "> AudioContext sample_rate: {:?}",
+        audio_context.sample_rate()
+    );
 
-    // --------------------------------------------------------------
-    println!("");
+    println!("--------------------------------------------------------------");
     println!("> Case 1: buffers are decoded at right sample rate by context");
-    println!("");
+    println!("--------------------------------------------------------------");
 
     let file_38000 = File::open("samples/sample-38000.wav").unwrap();
     let buffer_38000 = audio_context.decode_audio_data_sync(file_38000).unwrap();
@@ -21,7 +23,10 @@ fn main() {
     let buffer_48000 = audio_context.decode_audio_data_sync(file_48000).unwrap();
 
     // audio context at default system sample rate
-    println!("+ playing sample-38000.wav - decoded sample rate: {:?}", buffer_38000.sample_rate());
+    println!(
+        "+ playing sample-38000.wav - decoded sample rate: {:?}",
+        buffer_38000.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_38000);
@@ -29,7 +34,10 @@ fn main() {
 
     std::thread::sleep(std::time::Duration::from_millis(3500));
 
-    println!("+ playing sample-44100.wav - decoded sample rate: {:?}", buffer_44100.sample_rate());
+    println!(
+        "+ playing sample-44100.wav - decoded sample rate: {:?}",
+        buffer_44100.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_44100);
@@ -37,7 +45,10 @@ fn main() {
 
     std::thread::sleep(std::time::Duration::from_millis(3500));
 
-    println!("+ playing sample-48000.wav - decoded sample rate: {:?}", buffer_48000.sample_rate());
+    println!(
+        "+ playing sample-48000.wav - decoded sample rate: {:?}",
+        buffer_48000.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_48000);
@@ -46,11 +57,9 @@ fn main() {
     std::thread::sleep(std::time::Duration::from_millis(3500));
 
     // --------------------------------------------------------------
-    println!("");
-    println!("> Case 2:
-        buffers are decoded with another sample rate,
-        then resampled by the AudioBufferSourceNode");
-    println!("");
+    println!("--------------------------------------------------------------");
+    println!("> Case 2: buffers are decoded with another sample rate, then resampled by the AudioBufferSourceNode");
+    println!("--------------------------------------------------------------");
 
     let audio_context_38000 = AudioContext::new(Some(AudioContextOptions {
         sample_rate: Some(38000),
@@ -58,7 +67,9 @@ fn main() {
         channels: None,
     }));
     let file_38000 = File::open("samples/sample-38000.wav").unwrap();
-    let buffer_38000 = audio_context_38000.decode_audio_data_sync(file_38000).unwrap();
+    let buffer_38000 = audio_context_38000
+        .decode_audio_data_sync(file_38000)
+        .unwrap();
 
     let audio_context_44100 = AudioContext::new(Some(AudioContextOptions {
         sample_rate: Some(44100),
@@ -66,7 +77,9 @@ fn main() {
         channels: None,
     }));
     let file_44100 = File::open("samples/sample-44100.wav").unwrap();
-    let buffer_44100 = audio_context_44100.decode_audio_data_sync(file_44100).unwrap();
+    let buffer_44100 = audio_context_44100
+        .decode_audio_data_sync(file_44100)
+        .unwrap();
 
     let audio_context_48000 = AudioContext::new(Some(AudioContextOptions {
         sample_rate: Some(48000),
@@ -74,10 +87,15 @@ fn main() {
         channels: None,
     }));
     let file_48000 = File::open("samples/sample-48000.wav").unwrap();
-    let buffer_48000 = audio_context_48000.decode_audio_data_sync(file_48000).unwrap();
+    let buffer_48000 = audio_context_48000
+        .decode_audio_data_sync(file_48000)
+        .unwrap();
 
     // audio context at default system sample rate
-    println!("+ playing sample-38000.wav - decoded sample rate: {:?}", buffer_38000.sample_rate());
+    println!(
+        "+ playing sample-38000.wav - decoded sample rate: {:?}",
+        buffer_38000.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_38000);
@@ -85,7 +103,10 @@ fn main() {
 
     std::thread::sleep(std::time::Duration::from_millis(3500));
 
-    println!("+ playing sample-44100.wav - decoded sample rate: {:?}", buffer_44100.sample_rate());
+    println!(
+        "+ playing sample-44100.wav - decoded sample rate: {:?}",
+        buffer_44100.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_44100);
@@ -93,7 +114,10 @@ fn main() {
 
     std::thread::sleep(std::time::Duration::from_millis(3500));
 
-    println!("+ playing sample-48000.wav - decoded sample rate: {:?}", buffer_48000.sample_rate());
+    println!(
+        "+ playing sample-48000.wav - decoded sample rate: {:?}",
+        buffer_48000.sample_rate()
+    );
     let src = audio_context.create_buffer_source();
     src.connect(&audio_context.destination());
     src.set_buffer(buffer_48000);
