e780c9ff5cbcbb7c3920dafca8aa1b2e72f2b98e
Otto <ottorottier@gmail.com>
2022-03-20 11:18:59 +0100

    cargo fmt

--- src/media/decoding.rs
+++ src/media/decoding.rs
@@ -133,12 +133,12 @@ impl MediaDecoder {
         let format = probed.format;
 
         // Get the default track.
-        let track = format.default_track()
-            .ok_or(SymphoniaError::Unsupported("no default media track available"))?;
+        let track = format.default_track().ok_or(SymphoniaError::Unsupported(
+            "no default media track available",
+        ))?;
 
         // Create a (stateful) decoder for the track.
-        let decoder = symphonia::default::get_codecs()
-            .make(&track.codec_params, &decoder_opts)?;
+        let decoder = symphonia::default::get_codecs().make(&track.codec_params, &decoder_opts)?;
 
         Ok(Self { format, decoder })
     }
