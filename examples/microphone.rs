a0d0ea4821ae303307a59e290f194bee07b855b7
Otto <ottorottier@gmail.com>
2022-04-07 09:00:44 +0200

    Change the microphone audio source stream, we can now pause and resume

--- examples/microphone.rs
+++ examples/microphone.rs
@@ -6,12 +6,25 @@ fn main() {
     env_logger::init();
     let context = AudioContext::default();
 
-    let stream = Microphone::new();
+    let mic = Microphone::new();
     // register as media element in the audio context
-    let background = context.create_media_stream_source(stream);
+    let background = context.create_media_stream_source(mic.stream());
     // connect the node to the destination node (speakers)
     background.connect(&context.destination());
 
-    // enjoy listening
+    println!("Playback for 2 seconds");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    println!("Pause mic for 2 seconds");
+    mic.suspend();
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    println!("Resume mic for 2 seconds");
+    mic.resume();
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // Microphone is allowed to go out of scope, recording should continue
+    println!("Drop mic and resume for 4 seconds");
+    drop(mic);
     std::thread::sleep(std::time::Duration::from_secs(4));
 }
--- src/media/mic.rs
+++ src/media/mic.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
 
 use crate::buffer::{AudioBuffer, AudioBufferOptions};
+use crate::media::MediaStream;
 use crate::{SampleRate, RENDER_QUANTUM_SIZE};
 
 #[cfg(not(test))]
 use crossbeam_channel::Sender;
+#[cfg(not(test))]
+use std::sync::Arc;
 
 #[cfg(not(test))]
 use crate::buffer::ChannelData;
@@ -15,6 +18,27 @@ use crate::io;
 use cpal::{traits::StreamTrait, Sample, Stream};
 
 use crossbeam_channel::{Receiver, TryRecvError};
+
+#[cfg(not(test))]
+mod private {
+    use super::*;
+
+    pub struct StreamHolder(Arc<Stream>);
+
+    impl StreamHolder {
+        pub fn new(stream: Arc<Stream>) -> Self {
+            StreamHolder(stream)
+        }
+    }
+
+    // SAFETY: Stream is not Send, but we do want to have shared ownership via an Arc.
+    // The StreamHolder is simply keeping the Stream alive, it does not allow any mutation to it.
+    #[allow(clippy::non_send_fields_in_send_ty)]
+    unsafe impl Send for StreamHolder {}
+}
+
+#[cfg(not(test))]
+use private::StreamHolder;
 
 /// Microphone input stream
 ///
@@ -35,9 +59,9 @@ use crossbeam_channel::{Receiver, TryRecvError};
 ///
 /// let context = AudioContext::default();
 ///
-/// let stream = Microphone::new();
+/// let mic = Microphone::new();
 /// // register as media element in the audio context
-/// let background = context.create_media_stream_source(stream);
+/// let background = context.create_media_stream_source(mic.stream());
 /// // connect the node directly to the destination node (speakers)
 /// background.connect(&context.destination());
 ///
@@ -50,13 +74,8 @@ pub struct Microphone {
     sample_rate: SampleRate,
 
     #[cfg(not(test))]
-    stream: Stream,
+    stream: Arc<Stream>,
 }
-
-// Todo, the Microphone struct is shipped to the render thread
-// but it contains a Stream which is not Send.
-#[allow(clippy::non_send_fields_in_send_ty)]
-unsafe impl Send for Microphone {}
 
 impl Microphone {
     /// Setup the default microphone input stream
@@ -67,6 +86,10 @@ impl Microphone {
 
         let sample_rate = SampleRate(config.sample_rate.0);
         let number_of_channels = config.channels as usize;
+
+        // shared ownership for the stream, because the Microphone is allowed to go out of scope
+        // but all corresponding streams should still yield output
+        let stream = Arc::new(stream);
 
         Self {
             receiver,
@@ -102,6 +125,21 @@ impl Microphone {
         #[cfg(not(test))] // in tests, do not set up a cpal Stream
         self.stream.play().unwrap()
     }
+
+    /// A [`MediaStream`] iterator producing audio buffers from the microphone input
+    ///
+    /// Note that while you can call this function multiple times and poll all iterators
+    /// concurrently, this could lead to unexpected behavior as the buffers will only be offered
+    /// once.
+    pub fn stream(&self) -> impl MediaStream {
+        MicrophoneStream {
+            receiver: self.receiver.clone(),
+            number_of_channels: self.number_of_channels,
+            sample_rate: self.sample_rate,
+            #[cfg(not(test))]
+            _stream: StreamHolder::new(self.stream.clone()),
+        }
+    }
 }
 
 #[cfg(not(test))]
@@ -111,7 +149,19 @@ impl Default for Microphone {
     }
 }
 
-impl Iterator for Microphone {
+// no need for public documentation because the concrete type is never returned (an impl
+// MediaStream is returned instead)
+#[doc(hidden)]
+pub struct MicrophoneStream {
+    receiver: Receiver<AudioBuffer>,
+    number_of_channels: usize,
+    sample_rate: SampleRate,
+
+    #[cfg(not(test))]
+    _stream: StreamHolder,
+}
+
+impl Iterator for MicrophoneStream {
     type Item = Result<AudioBuffer, Box<dyn Error + Send + Sync>>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -122,7 +172,7 @@ impl Iterator for Microphone {
             }
             Err(TryRecvError::Empty) => {
                 // frame not received in time, emit silence
-                log::debug!("input frame delayed");
+                // log::debug!("input frame delayed");
 
                 let options = AudioBufferOptions {
                     number_of_channels: self.number_of_channels,
@@ -182,5 +232,12 @@ impl MicrophoneRender {
         if result.is_err() {
             log::debug!("input frame dropped");
         }
+    }
+}
+
+#[cfg(not(test))]
+impl Drop for MicrophoneRender {
+    fn drop(&mut self) {
+        log::debug!("Microphone input has been dropped");
     }
 }
