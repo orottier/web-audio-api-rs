30506d0e6489e83dbc75f7757c9c8f55faf2b5b0
Otto <ottorottier@gmail.com>
2022-07-04 21:11:59 +0200

    Change IIR example feed forward/backward values
    a bit more audible now

--- examples/iir.rs
+++ examples/iir.rs
@@ -8,15 +8,12 @@ fn main() {
     let file = File::open("samples/major-scale.ogg").unwrap();
     let buffer = context.decode_audio_data_sync(file).unwrap();
 
-    let feedforward = vec![
-        0.000016636797512844526,
-        0.00003327359502568905,
-        0.000016636797512844526,
-    ];
-    let feedback = vec![1.0, -1.9884300106225539, 0.9884965578126054];
+    // taken from https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_IIR_filters
+    let feed_forward = vec![0.00020298, 0.0004059599, 0.00020298];
+    let feed_backward = vec![1.0126964558, -1.9991880801, 0.9873035442];
 
     // Create an IIR filter node
-    let iir = context.create_iir_filter(feedforward, feedback);
+    let iir = context.create_iir_filter(feed_forward, feed_backward);
     iir.connect(&context.destination());
 
     // Play the buffer
