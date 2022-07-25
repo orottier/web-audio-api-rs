474c4a4943ab2fb796b1337c2cd6e73e788cb096
Otto <ottorottier@gmail.com>
2022-07-25 09:15:42 +0200

    Microphone input for `cubeb` - setting up a 2nd stream when requested
    This is not how it should be, we should link the input stream together
    with the output stream in one go. However, this means that we always
    capture mic input, even if the user does not want it - TODO

--- src/io/backend_cubeb.rs
+++ src/io/backend_cubeb.rs
@@ -5,6 +5,7 @@ use super::AudioBackend;
 
 use crate::buffer::AudioBuffer;
 use crate::context::AudioContextOptions;
+use crate::media::MicrophoneRender;
 use crate::message::ControlMessage;
 use crate::render::RenderThread;
 use crate::{AtomicF64, RENDER_QUANTUM_SIZE};
@@ -127,7 +128,7 @@ impl AudioBackend for CubebBackend {
                 // TODO can we avoid the temp buffer?
                 let mut tmp = [0.; 128 * 2];
                 renderer.render(&mut tmp, 0.);
-                for (f, t) in output.iter_mut().zip(tmp.chunks(2)) {
+                for (f, t) in output.iter_mut().zip(tmp.chunks(number_of_channels)) {
                     f.l = t[0];
                     f.r = t[1];
                 }
@@ -150,11 +151,82 @@ impl AudioBackend for CubebBackend {
         (backend, sender)
     }
 
-    fn build_input(_options: AudioContextOptions) -> (Self, Receiver<AudioBuffer>)
+    fn build_input(options: AudioContextOptions) -> (Self, Receiver<AudioBuffer>)
     where
         Self: Sized,
     {
-        todo!()
+        /* Set up a dedicated stream for input capturing
+         *
+         * This is not how it should be, we should link the input stream together
+         * with the output stream in one go. However, this means that we always
+         * capture mic input, even if the user does not want it - TODO
+         */
+
+        // Set up cubeb context
+        let ctx = Context::init(None, None).unwrap();
+
+        // Use user requested sample rate, or else the device preferred one
+        let device_sample_rate = ctx.preferred_sample_rate().map(|v| v as f32).ok();
+        let sample_rate = options.sample_rate.or(device_sample_rate).unwrap_or(48000.);
+
+        // TODO support all channel configs
+        let _max_channel_count = ctx.max_channel_count().map(|v| v as usize).ok();
+        let number_of_channels = 2;
+        let layout = cubeb::ChannelLayout::STEREO;
+
+        let params = cubeb::StreamParamsBuilder::new()
+            .format(cubeb::SampleFormat::Float32LE) // TODO may not be available for device
+            .rate(sample_rate as u32)
+            .channels(number_of_channels as u32)
+            .layout(layout)
+            .take();
+
+        // Calculate ideal latency
+        let buffer_size_req =
+            super::buffer_size_for_latency_category(options.latency_hint, sample_rate) as u32;
+        let min_latency = ctx
+            .min_latency(&params)
+            .ok()
+            .unwrap_or(RENDER_QUANTUM_SIZE as u32);
+        let buffer_size = buffer_size_req.max(min_latency);
+
+        let smoothing = 3; // todo, use buffering to smooth frame drops
+        let (sender, receiver) = crossbeam_channel::bounded(smoothing);
+        let renderer = MicrophoneRender::new(number_of_channels, sample_rate, sender);
+
+        let mut builder = cubeb::StreamBuilder::<Frame>::new();
+        builder
+            .name("Cubeb web_audio_api (mono)")
+            .default_output(&params)
+            .default_input(&params)
+            .latency(buffer_size)
+            .data_callback(move |input, _output| {
+                // TODO can we avoid the temp buffer?
+                let mut tmp = [0.; 128 * 2];
+                tmp.chunks_mut(number_of_channels)
+                    .zip(input)
+                    .for_each(|(t, i)| {
+                        t[0] = i.l;
+                        t[1] = i.r;
+                    });
+                renderer.render(&tmp);
+                input.len() as isize
+            })
+            .state_callback(|state| {
+                println!("stream state changed: {:?}", state);
+            });
+
+        let stream = builder.init(&ctx).expect("Failed to create cubeb stream");
+
+        stream.start().unwrap();
+
+        let backend = CubebBackend {
+            stream: ThreadSafeClosableStream::new(stream),
+            number_of_channels,
+            sample_rate,
+        };
+
+        (backend, receiver)
     }
 
     fn resume(&self) -> bool {
