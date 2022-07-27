435ad99fcb53e1a1ec26fa130a138b6688af60b4
b-ma <benjamin.matuszewski@gmail.com>
2022-07-27 18:12:31 +0200

    comsetics

--- examples/compressor.rs
+++ examples/compressor.rs
@@ -5,13 +5,13 @@ use web_audio_api::node::{AudioNode, AudioScheduledSourceNode, DynamicsCompresso
 fn main() {
     env_logger::init();
 
-    println!("> gradually increase the amount of compression applied on the sample");
 
     let context = AudioContext::default();
 
     let file = File::open("samples/sample.wav").unwrap();
     let buffer = context.decode_audio_data_sync(file).unwrap();
 
+    println!("> no compression");
     let src = context.create_buffer_source();
     src.connect(&context.destination());
     src.set_buffer(buffer.clone());
@@ -20,12 +20,19 @@ fn main() {
     // enjoy listening
     std::thread::sleep(std::time::Duration::from_secs(4));
 
-    for i in 0..7 {
+    println!("> compression (hard knee)");
+    println!("+ attack: {:?}ms", 30);
+    println!("+ release: {:?}ms", 100);
+    println!("+ ratio: {:?}", 12);
+    println!(">");
+
+    for i in 0..6 {
+        println!("+ threshold at {:?}", -10. * i as f32);
         let compressor = DynamicsCompressorNode::new(&context, Default::default());
         compressor.connect(&context.destination());
         compressor.threshold().set_value(-10. * i as f32);
         compressor.knee().set_value(0.); // hard knee
-        compressor.attack().set_value(0.05); // hard knee
+        compressor.attack().set_value(0.03); // hard knee
         compressor.release().set_value(0.1); // hard knee
 
         let src = context.create_buffer_source();
