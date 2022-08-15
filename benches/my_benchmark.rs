86fc3109196a3f266eadc269ca0362bb5befa4a9
Otto <ottorottier@gmail.com>
2022-08-15 12:23:28 +0200

    Add CI benchmarks for AudioBufferSourceNode and IIRFilterNode

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -51,6 +51,47 @@ pub fn bench_sine_gain_delay() {
     assert_eq!(ctx.start_rendering_sync().length(), 48000);
 }
 
+pub fn bench_buffer_src() {
+    let ctx = OfflineAudioContext::new(2, black_box(48000), 48000.);
+
+    let file = std::fs::File::open("samples/think-stereo-48000.wav").unwrap();
+    let buffer = ctx.decode_audio_data_sync(file).unwrap();
+
+    let src = ctx.create_buffer_source();
+    src.connect(&ctx.destination());
+    src.set_buffer(buffer);
+    src.start();
+
+    assert_eq!(ctx.start_rendering_sync().length(), 48000);
+}
+
+pub fn bench_buffer_src_iir() {
+    let ctx = OfflineAudioContext::new(2, black_box(48000), 48000.);
+    let file = std::fs::File::open("samples/think-stereo-48000.wav").unwrap();
+    let buffer = ctx.decode_audio_data_sync(file).unwrap();
+
+    // these values correspond to a lowpass filter at 200Hz (calculated from biquad)
+    let feedforward = vec![
+        0.0002029799640409502,
+        0.0004059599280819004,
+        0.0002029799640409502,
+    ];
+
+    let feedback = vec![1.0126964557853775, -1.9991880801438362, 0.9873035442146225];
+
+    // Create an IIR filter node
+    let iir = ctx.create_iir_filter(feedforward, feedback);
+    iir.connect(&ctx.destination());
+
+    // Play buffer and pipe to filter
+    let src = ctx.create_buffer_source();
+    src.connect(&iir);
+    src.set_buffer(buffer);
+    src.start();
+
+    assert_eq!(ctx.start_rendering_sync().length(), 48000);
+}
+
 iai::main!(
     bench_ctor,
     bench_sine,
