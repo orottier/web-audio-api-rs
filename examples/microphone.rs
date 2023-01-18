4a669943640b0725752b81638a6c2c3f86b4d623
b-ma <benjamin.matuszewski@gmail.com>
2023-01-18 08:52:27 +0100

    doc: simplify microphone example

--- examples/microphone.rs
+++ examples/microphone.rs
@@ -4,28 +4,33 @@ use web_audio_api::node::AudioNode;
 
 fn main() {
     env_logger::init();
+
     let context = AudioContext::default();
-
     let mic = Microphone::default();
-    // register as media element in the audio context
-    let background = context.create_media_stream_source(mic.stream());
-    // connect the node to the destination node (speakers)
-    background.connect(&context.destination());
 
-    println!("Playback for 2 seconds");
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    // create media stream source node with mic stream
+    let stream_source = context.create_media_stream_source(mic.stream());
+    stream_source.connect(&context.destination());
 
-    println!("Pause mic for 2 seconds");
-    mic.suspend();
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(2));
+    }
 
-    println!("Resume mic for 2 seconds");
-    mic.resume();
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    // note: uncomment to test controls over the mic instance,
+    // this is maybe not the desired public interface, see MediaDevices API
 
-    // Closing the mic should halt the media stream source
-    println!("Close mic - halting stream");
-    mic.close();
+    // println!("Playback for 2 seconds");
+    // std::thread::sleep(std::time::Duration::from_secs(2));
 
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    // println!("Pause mic for 2 seconds");
+    // mic.suspend();
+    // std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // println!("Resume mic for 2 seconds");
+    // mic.resume();
+    // std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // // Closing the mic should halt the media stream source
+    // println!("Close mic - halting stream");
+    // mic.close();
 }
