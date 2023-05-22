1fbe066b910236c8d5523b1717e51541f5af4ed7
b-ma <benjamin.matuszewski@gmail.com>
2023-05-22 20:01:18 +0200

    undo add space

--- src/media_devices/mod.rs
+++ src/media_devices/mod.rs
@@ -20,7 +20,6 @@ use crate::media_streams::MediaStream;
 /// assert_eq!(devices[0].kind(), MediaDeviceInfoKind::AudioOutput);
 /// assert_eq!(devices[0].label(), "Macbook Pro Builtin Speakers");
 /// ```
-
 pub fn enumerate_devices() -> Vec<MediaDeviceInfo> {
     crate::io::enumerate_devices()
 }
