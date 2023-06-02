01bfc1f290e93bdb6a9cdbf4dc5ae2ea476fe89a
b-ma <benjamin.matuszewski@gmail.com>
2023-06-02 10:28:04 +0200

    example: allow choosing source_id and sink_id

--- examples/microphone.rs
+++ examples/microphone.rs
@@ -1,10 +1,10 @@
-use web_audio_api::context::{AudioContext, BaseAudioContext};
+use web_audio_api::context::{AudioContext, AudioContextOptions, BaseAudioContext};
 use web_audio_api::media_devices;
 use web_audio_api::media_devices::{enumerate_devices_sync, MediaDeviceInfo, MediaDeviceInfoKind};
 use web_audio_api::media_devices::{MediaStreamConstraints, MediaTrackConstraints};
 use web_audio_api::node::AudioNode;
 
-fn ask_sink_id() -> Option<String> {
+fn ask_source_id() -> Option<String> {
     println!("Enter the input 'device_id' and press <Enter>");
     println!("- Use 0 for the default audio input device");
 
@@ -15,21 +15,47 @@ fn ask_sink_id() -> Option<String> {
     }
 }
 
+fn ask_sink_id() -> String {
+    println!("Enter the input 'sink' and press <Enter>");
+    println!("- Use 0 for the default audio input device");
+
+    let input = std::io::stdin().lines().next().unwrap().unwrap();
+    match input.trim() {
+        "0" => "none".to_string(),
+        i => i.to_string(),
+    }
+}
+
 fn main() {
     env_logger::init();
 
     let devices = enumerate_devices_sync();
+
     let input_devices: Vec<MediaDeviceInfo> = devices
         .into_iter()
         .filter(|d| d.kind() == MediaDeviceInfoKind::AudioInput)
         .collect();
 
     dbg!(input_devices);
+    let source_id = ask_source_id();
+
+    let devices = enumerate_devices_sync();
+
+    let output_devices: Vec<MediaDeviceInfo> = devices
+        .into_iter()
+        .filter(|d| d.kind() == MediaDeviceInfoKind::AudioOutput)
+        .collect();
+
+    dbg!(output_devices);
     let sink_id = ask_sink_id();
 
-    let context = AudioContext::default();
+    let context = AudioContext::new(AudioContextOptions {
+        sink_id,
+        ..AudioContextOptions::default()
+    });
+
     let mut constraints = MediaTrackConstraints::default();
-    constraints.device_id = sink_id;
+    constraints.device_id = source_id;
     let stream_constraints = MediaStreamConstraints::AudioWithConstraints(constraints);
     let mic = media_devices::get_user_media_sync(stream_constraints);
 
@@ -37,10 +63,11 @@ fn main() {
     let stream_source = context.create_media_stream_source(&mic);
     stream_source.connect(&context.destination());
 
-    println!("Recording for 5 seconds");
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 
-    println!("Closing microphone");
-    mic.get_tracks()[0].close();
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    // println!("Closing microphone");
+    // mic.get_tracks()[0].close();
+    // std::thread::sleep(std::time::Duration::from_secs(2));
 }
