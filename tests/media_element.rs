4e02245259492c17c007d880d0e31ec5d2901891
Otto <ottorottier@gmail.com>
2023-07-16 09:13:24 +0200

    Add integration test for MediaElementSourceNode

---
+++ tests/media_element.rs
@@ -0,0 +1,25 @@
+use web_audio_api::context::{AudioContext, AudioContextOptions, BaseAudioContext};
+use web_audio_api::node::AudioNode;
+use web_audio_api::MediaElement;
+
+#[test]
+fn test_media_element_source_progress() {
+    let options = AudioContextOptions {
+        sink_id: "none".into(),
+        ..AudioContextOptions::default()
+    };
+    let context = AudioContext::new(options);
+
+    let mut media = MediaElement::new("samples/major-scale.ogg").unwrap();
+    media.set_loop(true);
+
+    let src = context.create_media_element_source(&mut media);
+    src.connect(&context.destination());
+    media.play();
+
+    // sleep for a bit, make sure the audio thread has started
+    std::thread::sleep(std::time::Duration::from_millis(100));
+
+    // assert the media has progressed
+    assert!(media.current_time() > 0.05);
+}
