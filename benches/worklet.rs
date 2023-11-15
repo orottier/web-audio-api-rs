2c14d293f58536cf55e56d82fe071960a9abe5f4
Otto <ottorottier@gmail.com>
2023-11-15 08:36:29 +0100

    Update benches/worklet implementation to match native performance

--- benches/worklet.rs
+++ benches/worklet.rs
@@ -31,16 +31,22 @@ impl AudioWorkletProcessor for GainProcessor {
         outputs: &'b mut [&'a mut [&'a mut [f32]]],
         params: AudioParamValues<'b>,
     ) -> bool {
-        // passthrough with gain
-        inputs[0]
-            .iter()
-            .zip(outputs[0].iter_mut())
-            .for_each(|(ic, oc)| {
-                let gain = params.get("gain");
+        let gain = params.get("gain");
+        let io_zip = inputs[0].iter().zip(outputs[0].iter_mut());
+        if gain.len() == 1 {
+            let gain = gain[0];
+            io_zip.for_each(|(ic, oc)| {
+                for (is, os) in ic.iter().zip(oc.iter_mut()) {
+                    *os = is * gain;
+                }
+            });
+        } else {
+            io_zip.for_each(|(ic, oc)| {
                 for ((is, os), g) in ic.iter().zip(oc.iter_mut()).zip(gain.iter().cycle()) {
                     *os = is * g;
                 }
             });
+        }
 
         false
     }
