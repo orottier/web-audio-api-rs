07f354f552584e9dd15d6b77a5649c11acf761ed
b-ma <benjamin.matuszewski@gmail.com>
2023-11-22 23:17:34 +0100

    test case to starve cpu

---
+++ examples/starving_cpu.rs
@@ -0,0 +1,24 @@
+use web_audio_api::context::{AudioContext, BaseAudioContext};
+use web_audio_api::node::{AudioNode, AudioScheduledSourceNode, OscillatorNode};
+
+fn main() {
+    env_logger::init();
+
+    let context = AudioContext::default();
+
+    let mut osc: Option<OscillatorNode> = None;
+
+    loop {
+        if let Some(prev_osc) = osc {
+            prev_osc.disconnect();
+        }
+
+        let mut new_osc = context.create_oscillator();
+        new_osc.connect(&context.destination());
+        new_osc.start();
+
+        osc = Some(new_osc);
+
+        std::thread::sleep(std::time::Duration::from_millis(20));
+    }
+}
