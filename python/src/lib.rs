a97fdd9eace3153dd0b083debd7d27c765b0cad2
Otto <ottorottier@gmail.com>
2024-06-08 16:56:46 +0200

    It's alive

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,8 +1,16 @@
 use pyo3::prelude::*;
 
+use web_audio_api_rs::context::{AudioContext, BaseAudioContext};
+use web_audio_api_rs::node::{AudioNode, AudioScheduledSourceNode};
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    let ctx = AudioContext::default();
+    let mut osc = ctx.create_oscillator();
+    osc.connect(&ctx.destination());
+    osc.start();
+
     Ok((a + b).to_string())
 }
 
